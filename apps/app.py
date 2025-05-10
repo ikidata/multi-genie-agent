8f1a9812b6b0c50fefb4dd193102864589323c76
ikidata <ikidata>
2025-05-10 12:40:29 +0000

    Updated OpenAI client to be used from SDK instead of PAT token
    Now that SDK supports openai client fetching, key vault parameters are unnecessary

--- apps/app.py
+++ apps/app.py
@@ -8,18 +8,12 @@ import flask  # for request context
 serving_endpoint = os.getenv('SERVING_ENDPOINT')
 assert serving_endpoint, 'SERVING_ENDPOINT must be set in app.yaml.'
 
-databricks_host_secret = os.getenv('DATABRICKS_HOST_SECRET')
-assert databricks_host_secret, 'DATABRICKS_HOST_SECRET must be set in app.yaml.'
-
-databricks_token_secret = os.getenv('DATABRICKS_TOKEN_SECRET')
-assert databricks_token_secret, 'DATABRICKS_TOKEN_SECRET must be set in app.yaml.'
-
 # Initialize the Dash app with a clean theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
 
 # Instantiate chatbot without layout
-chatbot = DatabricksChatbot(app=app, endpoint_name=serving_endpoint, databricks_host_secret = databricks_host_secret, databricks_token_secret = databricks_token_secret, height='600px')
+chatbot = DatabricksChatbot(app=app, endpoint_name=serving_endpoint, height='600px')
 
 # Use layout function (important!)
 def serve_layout():
